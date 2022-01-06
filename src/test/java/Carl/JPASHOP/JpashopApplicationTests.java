5f5cf4463ba26ae29576ed0a247eff9f2ad9110d
Hwan <mirrorkyh@gmail.com>
2022-01-06 14:07:26 +0900

    JPA SHOP

---
+++ src/test/java/Carl/JPASHOP/JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package Carl.JPASHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
