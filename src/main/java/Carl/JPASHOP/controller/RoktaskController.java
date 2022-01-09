ea107835491a35308b7d803bcd08c0e465a80dbe
Hwan <mirrorkyh@gmail.com>
2022-01-10 08:55:48 +0900

    JPA SHOP sanae

--- src/main/java/Carl/JPASHOP/controller/RoktaskController.java
+++ src/main/java/Carl/JPASHOP/controller/RoktaskController.java
@@ -19,7 +19,7 @@ public class RoktaskController {
     @Autowired // 오토와이어드 작업 안하면 빈 주입 안되어서 작동 안한다. 레파지토리에 빈 주입
     HwanRepository hwanRepository;
 
-    @GetMapping(value = "/create")
+    @GetMapping(value = "/create") // select
     public void create() {
         Roktask roktask = new Roktask();
         roktask.setName("Hwan");
@@ -27,14 +27,36 @@ public class RoktaskController {
         Roktask newRoktask = hwanRepository.save(roktask);
     }
 
+    //
+
     @GetMapping(value = "/find")
-    public List<Roktask> findByname() {
-//        this.creattalbe();
+    public List<Roktask> findByname(
+            @RequestParam(value = "name")String name,
+            @RequestParam(value = "phonenumber")String phonenumber
+    ) {
+        this.create();
         List<Roktask> roktaskList = hwanRepository.findByname("Hwan");
         for (Roktask roktask : roktaskList) {
             System.out.println(roktask.toString()); // 조회로 얻은 객체 한 줄 출력하기
         }
         return roktaskList;
+    }
+
+
+    @PostMapping(value = "/http/get") // insert
+    public String updatTest(){
+        return "insert 요청";
+    }
+    // 인터넷 브라우저 요청은 get요청 밖에 안돼.
+    // update, delete, read 다 안됨. 오로지 겟만
+    @PutMapping(value = "/http/put")  // update
+    public String putTest() {
+        return "update 요청";
+    }
+
+    @DeleteMapping(value = "/http/delete") // delete
+    public String deleteTest() {
+        return "delete 요청";
     }
 }
 //    @GetMapping(value = "/insert")
@@ -45,11 +67,3 @@ public class RoktaskController {
 //        Roktask saveRoktask = hwanRepository.save(roktask);
 //
 //        return saveRoktask;
-
-
-
-
-
-
-
-
